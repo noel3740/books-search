aad242cbde9ef3fbb55e11b9a9a6b96eecc4c422
Noel Nevarez <noel3740@gmail.com>
2019-03-06 14:25:43 -0600

    Adding initial saved and search pages.

---
+++ client/src/pages/Saved/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+class Saved extends React.Component {
+    render() {
+        return (
+            <div className="savedPage">
+                <h1>This is the saved page</h1>
+            </div>
+        )
+    }
+}
+
+export default Saved;
---
+++ client/src/pages/Search/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div className="searchPage">
+                <h1>This is the search page</h1>
+            </div>
+        )
+    }
+}
+
+export default Search;
