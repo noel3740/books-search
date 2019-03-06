61b76dc97dfb6121a9b6ded38eb59eac9be8478c
Noel Nevarez <noel3740@gmail.com>
2019-03-06 14:51:32 -0600

    Adding router for the different pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Nav />
+      <BrowserRouter>
+        <div className="App">
+          <Nav />
 
-        <Jumbotron
-          mainText="Book Search"
-          detailText="Search for and save books of interest"
-        />
-        
-      </div>
+          <Jumbotron
+            mainText="Book Search"
+            detailText="Search for and save books of interest"
+          />
+
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route component={Search} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
