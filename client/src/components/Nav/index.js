7d0152540b998e0f3a8bf0e0ae996808cbae3ebe
Noel Nevarez <noel3740@gmail.com>
2019-03-06 15:47:24 -0600

    Changning nav links to react-router-dom links.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './style.css';
 
 class Nav extends React.Component {
 
+    sideNavInstance;
+
     componentDidMount = () => {
         //Initialize the nav bar
-        window.$('.sidenav').sidenav();
+        const sideNavElement = document.querySelector('.sidenav');
+        this.sideNavInstance = window.M.Sidenav.init(sideNavElement);
+    };
+
+    //Method that closes the side nav
+    closeSideNav = () => {
+        this.sideNavInstance.close();
     };
 
     render() {
@@ -18,8 +27,12 @@ class Nav extends React.Component {
                             <a href="/" className="brand-logo"><i className="bookLogo"></i> Book Search</a>
                             <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                             <ul className="right hide-on-med-and-down">
-                                <li className={ window.location.pathname.toLowerCase() !== "/saved" ? "active" : "" }><a href="/">Search</a></li>
-                                <li className={ window.location.pathname.toLowerCase() === "/saved" ? "active" : "" }><a href="/saved">Saved</a></li>
+                                <li className={window.location.pathname.toLowerCase() !== "/saved" ? "active" : ""}>
+                                    <Link to={"/"}>Search</Link>
+                                </li>
+                                <li className={window.location.pathname.toLowerCase() === "/saved" ? "active" : ""}>
+                                    <Link to={"/saved"}>Saved</Link>
+                                </li>
                             </ul>
                         </div>
                     </nav>
@@ -27,8 +40,12 @@ class Nav extends React.Component {
                 </div>
 
                 <ul className="sidenav" id="mobile-demo">
-                    <li className={ window.location.pathname.toLowerCase() !== "/saved" ? "active" : "" }><a href="/">Search</a></li>
-                    <li className={ window.location.pathname.toLowerCase() === "/saved" ? "active" : "" }><a href="/saved">Saved</a></li>
+                    <li className={window.location.pathname.toLowerCase() !== "/saved" ? "active" : ""}>
+                        <Link to={"/"} onClick={this.closeSideNav}>Search</Link>
+                    </li>
+                    <li className={window.location.pathname.toLowerCase() === "/saved" ? "active" : ""}>
+                        <Link to={"/saved"} onClick={this.closeSideNav}>Saved</Link>
+                    </li>
                 </ul>
 
             </div>
