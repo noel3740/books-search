c12f948bfa55c037ec017aad936380cfaa6ea808
Noel Nevarez <noel3740@gmail.com>
2019-03-07 05:21:51 -0600

    Modifying look of card component.

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -4,10 +4,24 @@ import './style.css';
 const Card = props => {
 
     return (
-        <div className="reactCard col s12 m6 l3" >
-            <div className="card hoverable waves-effect waves-dark" onClick= { () => props.onClickMethod(props.imageUrl) }>
-                <div className="card-image">
-                    <img className="reactCardImage" src={props.imageUrl} alt={props.imageText} />
+        <div className="reactCard row">
+            <div className="col s12">
+                <div className="card horizontal horizontalCard">
+                    <div className="card-image">
+                        <img alt={props.title} src={props.image} />
+                    </div>
+                    <div className="card-stacked">
+                        <div className="card-content">
+                            <span className="card-title">{props.title}</span>
+                            <p><small>by {props.author}</small></p>
+                            <br/>
+                            <p>{props.description}</p>
+                        </div>
+                        <div className="card-action">
+                            <a data-book-id={props.bookId} href={props.previewLink} target="_blank" rel="noopener noreferrer" className="marginRight waves-effect waves-light btn btn-small"><i className="material-icons left">visibility</i>view</a>
+                            <a data-book-id={props.bookId} href="#!" className="waves-effect waves-light btn btn-small"><i className="material-icons left">save</i>save</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
