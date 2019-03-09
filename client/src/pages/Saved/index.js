f73e50c60089f100341fc3c4311f0cd5db79fc36
Noel Nevarez <noel3740@gmail.com>
2019-03-09 10:55:41 -0600

    addinging code to saved page

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './style.css';
+import Card from '../../components/Card'
 
 class Saved extends React.Component {
+    state = {
+        books: []
+    }
+
     render() {
         return (
             <div className="container savedPage">
-                <h1>This is the saved page</h1>
+                <div className="row">
+                    <h4>Saved Books</h4>
+                    {this.state.books.map(book =>
+                        <Card
+                            isSaved="true"
+                            key={book.id}
+                            bookId={book.id}
+                            image={book.smallThumbnail}
+                            previewLink={book.infoLink}
+                            title={book.title}
+                            author={book.authors.join(',')}
+                            description={book.description} />
+                    )}
+                </div>
             </div>
         )
     }
