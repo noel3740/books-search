5c868eae62d00b3bb26bbccbfe89bb39d4be5a49
Noel Nevarez <noel3740@gmail.com>
2019-03-16 12:54:36 -0500

    Moving socket io listener initialization to index

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Socket listener
+window.ioSocket.on('message', function (msg) {
+    window.M.toast({ html: msg });
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
